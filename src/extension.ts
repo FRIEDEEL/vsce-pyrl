43eef08061586f199c1bb47779d565a76c7d1e7b
FRIEDEEL <983785793@qq.com>
2024-01-18 12:35:41 +0800

    fix: fix that the extension can't run script properly.
    The extension couldn't run the script properly because that
    child_process module execute command under the root dir `/`,
    so the relative path didn't lead to the correct path of the script.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
+import * as path from 'path';
 
 
 // This method is called when your extension is activated
@@ -11,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pyrl" is now active!');
-	let replaceLine = vscode.commands.registerCommand('helloworld.replaceLine',
+	let replaceLine = vscode.commands.registerCommand('pyrl.replaceLine',
 	() => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
@@ -20,8 +21,9 @@ export function activate(context: vscode.ExtensionContext) {
 			const lineContent=currentLine.text;
 
 			// 调用 Python 脚本并传递当前行的内容
-			const pythonScriptPath = '/src/python-script/supsubscript-replace.py';
+			const pythonScriptPath = path.join(context.extensionPath,'/src/python-script/supsubscript-replace.py');
 			const pythonCommand = `python "${pythonScriptPath}" "${lineContent}"`;
+			// const pythonCommand = `echo "${pythonScriptPath}$"`;
 			cp.exec(pythonCommand, (error, stdout, stderr) => {
 				if (error) {
 					console.error(`Error: ${error.message}`);
