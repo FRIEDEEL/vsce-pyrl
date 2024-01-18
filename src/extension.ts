5624bf1f3e442f2e96982f95a5b9d6641ebde5c9
FRIEDEEL <983785793@qq.com>
2024-01-18 11:39:27 +0800

    build(src/extension.ts): Construct core code of extension
    Add the main functional code in `extension.ts`.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,17 +11,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pyrl" is now active!');
+	let replaceLine = vscode.commands.registerCommand('helloworld.replaceLine',
+	() => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const selection = editor.selection;
+			const currentLine = editor.document.lineAt(selection.start.line);
+			const lineContent=currentLine.text;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pyrl.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pyrl!');
+			// 调用 Python 脚本并传递当前行的内容
+			const pythonScriptPath = '/Users/liguang/测试code游乐场/240106/for_vscode_extension.py';
+			const pythonCommand = `python "${pythonScriptPath}" "${lineContent}"`;
+			cp.exec(pythonCommand, (error, stdout, stderr) => {
+				if (error) {
+					console.error(`Error: ${error.message}`);
+					vscode.window.showErrorMessage(`Error: ${error.message}`);
+				} else {
+					// 将 Python 脚本的输出替换回当前行
+					editor.edit((editBuilder) => {
+						editBuilder.replace(currentLine.range, stdout.trim());
+					});
+				}
+			});
+		} else {
+			vscode.window.showErrorMessage('No active text editor.');
+		}
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(replaceLine);
 }
 
 // This method is called when your extension is deactivated
